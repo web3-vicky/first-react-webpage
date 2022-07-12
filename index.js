f572406370966db7b81d107bbd6c8109acca14a9
web3 vicky <vicky.web3@gmail.com>
2022-07-12 12:42:10 +0530

    commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const nav = (
     <div>
-        <h1>My webpage</h1>
-        <p>All details are shown here</p>
+        <img src="C:\Users\Vignesh Murugan\first react webpage\first-react-webpage\react-logo.png" width= "300px" height= "200px" />
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>JavaScript</li>
-            <li>HTML</li>
-            <li>CSS</li>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
     </div>
 )
+
 ReactDOM.render(nav, document.getElementById("root") )
