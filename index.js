b183714cc0c40719558274582c2fe8e83c843910
web3 vicky <vicky.web3@gmail.com>
2022-07-12 21:27:32 +0530

    commit

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-import Header from "./Header.jsx"
+
+
+function Header() {
+    return(
+        <header>
+            <nav className="nav">
+                        <img src="./react-logo.jpg" alt="React Logo" height="80px" width="120px" />
+                        <ul className="nav-items">
+                            <li>Courses</li>
+                            <li>Pricing</li>
+                            <li>About</li>
+                        </ul>
+                    </nav>
+                </header>
+            )
+}
 
 function Footer() {
     return (
