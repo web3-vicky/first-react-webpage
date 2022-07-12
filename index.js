5bbb62d9312a83e2da3b5991b857ddf7a8892ec6
web3 vicky <vicky.web3@gmail.com>
2022-07-12 13:23:09 +0530

    commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
+    <div>
+        <img src="C:/Users/Vignesh Murugan/first react webpage/first-react-webpage/react-logo.jpg" alt="React Logo" height="200px" width="400px" />
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-    </nav>
+    </div>
 )
 
 ReactDOM.render(navbar, document.getElementById("root"))
