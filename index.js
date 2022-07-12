897557ec546374af02ce8e8fb821d6774b9df725
web3 vicky <vicky.web3@gmail.com>
2022-07-12 15:17:35 +0530

    commit

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+import Header from "./Header"
+import Footer from "./Footer"
+
+
+function MainContent() {
+    return(
+        <div>
+            <h1>Fun facts about React</h1>
+            <ul className="MainContent">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
+
 function page() {
     return (
         <div>
-        <img src="./react-logo.jpg" alt="React Logo" height="80px" width="120px" />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
     )
 }
 
