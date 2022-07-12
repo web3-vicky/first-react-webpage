64b20ad2e8ab5f76b4d34fd9a33056ec38af0dcb
web3 vicky <vicky.web3@gmail.com>
2022-07-12 13:03:36 +0530

    commit

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const nav = (
-    <div>
-        <img src="C:\Users\Vignesh Murugan\first react webpage\first-react-webpage\react-logo.png" width= "300px" height= "200px" />
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
-
-ReactDOM.render(nav, document.getElementById("root") )
+ReactDOM.render(<h1>Hello guys</h1>, document.getElementById("root") )
