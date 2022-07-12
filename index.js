e8165f45b7dd27fdb8e21e78df0b79d23bfd8173
web3 vicky <vicky.web3@gmail.com>
2022-07-12 12:44:40 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const nav = (
     <div>
         <img src="C:\Users\Vignesh Murugan\first react webpage\first-react-webpage\react-logo.png" width= "300px" height= "200px" />
-        <h1>Fun facts about React</h1>
         <ul>
             <li>Was first released in 2013</li>
             <li>Was originally created by Jordan Walke</li>
