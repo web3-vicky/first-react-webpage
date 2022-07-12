65d30bc9f27f63b28ed9ab06d9b642c704c1061a
web3 vicky <vicky.web3@gmail.com>
2022-07-12 11:44:06 +0530

    commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function customCode() {
-    return( 
+const nav = (
     <div>
-        <h1>Navigation pane</h1>
-    </div> )
-}
-
-
-
-
-ReactDOM.render(<customCode />, document.getElementById("root") )
+        <h1>My webpage</h1>
+        <p>All details are shown here</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+        </ul>
+    </div>
+)
+ReactDOM.render(nav, document.getElementById("root") )
